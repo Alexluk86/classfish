a0f4e2b60c103db8c6fc687910b767def4eee537
Alexluk86 <63222193+Alexluk86@users.noreply.github.com>
2020-06-17 11:15:08 +0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishClass
+{
+    class Fish
+    {
+        public double price { get; set; }
+        public int mass { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetFishInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {mass}| |Место: {place}|");
+        }
+    }
+
+
+    class Fish1 : Tree
+    {
+        public string type { get; set; }
+
+        public virtual void GetFish1Info()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Место: {place}| |Тип: {type}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fish fish = new fish();
+            fish.name = "Сом";
+            fish.price = 45;
+            fish.age = 30;
+            fish.place = "Озеро";
+
+            fish.GetfishInfo();
+
+            Fish1 fish1 = new Fish1();
+            fish1.type = "Осетровые";
+            fish1.name = "Осетр";
+            fish1.place = "Реки";
+            fish1.price = 100;
+            fish1.mass = 7;
+
+
+            fish1.GetFish1Info();
+
+            Console.ReadLine();
+        }
+    }
+}
